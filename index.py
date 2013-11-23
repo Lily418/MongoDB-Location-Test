d86203cc8f681cc3dd2988f897e024bb5eec09bc
Lily Hoskin <lilyhoskin@gmail.com>
2013-11-23 16:17:54 +0000

    User add and update

--- index.py
+++ index.py
@@ -1,10 +1,39 @@
-import os
-from flask import Flask
+from flask import Flask, request
+import json
+from flask.ext.pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+mongo = PyMongo(app)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+#curl -H "Content-Type: application/json" --data '{"user":"Joel"}' http://127.0.0.1:5000/user/
+@app.route('/user/', methods=['GET', 'POST'])
+def user():
+	if request.method == "POST":
+		data = json.loads(request.data)
+	        id = mongo.db.users.insert(data)	
+		return json.dumps({"id": str(id)}) 	
+	elif request.method == "GET":
+		users = []
+		for user in mongo.db.users.find():
+			user['_id'] = str(user['_id'])
+			users.append(user)
+
+		return json.dumps(users)
+
+@app.route('/user/<id>', methods=['POST', 'GET'])
+def userid(id):
+	if request.method == "POST":
+		mongo.db.users.update({'_id': ObjectId(id)}, {"$set": json.loads(request.data)})
+		return ""
+	elif request.method == "GET":
+		record = mongo.db.users.find_one({"_id" : ObjectId(id)})
+		record["_id"] = str(record["_id"])
+		return json.dumps(record)
+	 
 
 
+
+
+if __name__ == "__main__":
+	app.run(debug=True, port=5000)
